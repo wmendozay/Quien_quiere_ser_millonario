937bdfb6826aac552e64f97af33ee9c2f49283d9
wmendozay <wilsonmeya2008@hotmail.com>
2015-09-19 20:05:49 -0500

    se crean los atributos nombre,edad,correo se crean los diferentes metodos get y set

--- TrabajoF/src/Bases/Persona.java
+++ TrabajoF/src/Bases/Persona.java
@@ -10,5 +10,50 @@ package Bases;
  * @author sistemas
  */
 public class Persona {
-    
+    private String Nombre;
+	private String Correo;
+	private Integer Edad;
+	private String TipoUsuario;
+        
+        //setters
+	public void setNombre(String value)
+	{
+		this.Nombre=value;
+	}
+	
+	public void setCorreo(String value)
+	{
+		this.Correo=value;
+	}
+	
+	public void setEdad(Integer value)
+	{
+		this.Edad=value;
+	}
+	
+	public void setTipoUsuario(String value)
+	{
+		this.TipoUsuario=value;
+	}
+	
+	//getters
+	public String getNombre()
+	{
+		return this.Nombre;
+	}
+	
+	public String getCorreo()
+	{
+		return this.Correo;
+	}
+	
+	public Integer getEdad()
+	{
+		return this.Edad;
+	}
+	
+	public String getTipoUsuario()
+	{
+		return this.TipoUsuario;
+	}
 }
