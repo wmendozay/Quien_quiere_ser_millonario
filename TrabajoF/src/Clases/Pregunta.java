b867076583c198cda078b1485630de66f53efed5
deka31 <de-ka31@hotmail.com>
2015-09-21 21:17:40 +0200

    Se crean los atributos de la clase pregunta, se crea un arreglo para guardas las respuestas incorrectas

--- TrabajoF/src/Clases/Pregunta.java
+++ TrabajoF/src/Clases/Pregunta.java
@@ -1,14 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Clases;
 
+import java.util.ArrayList;
+import java.util.List;
 /**
  *
  * @author Usuario
  */
 public class Pregunta {
+    
+        public String Tematica;
+        public Integer NivelComplejidad;
+        public String TextoPregunta;
+        
+        public String RespuestaCorrecta;
+        public List<String> RespuestasIncorrectas=new ArrayList<String>();
     
 }
