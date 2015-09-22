9fabac14b97a3a935b2ab8f21b94a7ff4ccb6dcc
deka31 <de-ka31@hotmail.com>
2015-09-22 21:49:10 +0200

    En la clase Ayuda5050 se genera un número aleatorio entre 0 y 2 (Las tres respuestas malas) para descartarlo

--- TrabajoF/src/Clases/Ayuda5050.java
+++ TrabajoF/src/Clases/Ayuda5050.java
@@ -1,14 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Clases;
+import Bases.Acciones;
+import Interfaces.IAccion; 
+import java.util.Random;
 
 /**
  *
  * @author Usuario
  */
-public class Ayuda5050 {
+public class Ayuda5050 extends Acciones implements IAccion {
     
+    
+    public Ayuda5050 ()
+    {
+        this.NombreAccion= "50 \ 50";
+    }
+        public void EjecutarAccion(Pregunta pregunta) {
+    
+    
+                Random rnd = new Random();
+		int nmAleatorio1=(int)(rnd.nextDouble() * 3 + 0);
+		int nmAleatorio2=(int)(rnd.nextDouble() * 3 + 0);
+		
+		//mientras numero aleatorio 2 sea igual a num aleatorio 1, seguimos obteniendo un nuevo n�mero aleatorio
+		while (nmAleatorio2==nmAleatorio1) {
+			nmAleatorio2=(int)(rnd.nextDouble() * 3 + 0);
+		}
+		
+		//procedemos a eliminar las 2 respuestas erroneas;
+		pregunta.RespuestasIncorrectas.remove(nmAleatorio1);
+		pregunta.RespuestasIncorrectas.remove(nmAleatorio2);
+		
+		
+	}
+
+	
+
 }
+
